d79d6bfadd057e48ac12ea21e743fcfe25f2f26f
Alexandru Damian Manea <maneaalexdamian@gmail.com>
2019-03-20 02:09:37 +0200

    Fix websocketd binaries and and vJoy autosetup (windows)
    - use websocketd 32bit binaries as everything is 32bit for windows
    - use CRLF instead of LF in batch scripts
    - add vJoy autosetup and autoconfiguration of 16 controllers (takes a
    while, but it works)
    - remove linux executable flag from EXEs

--- dist/windows/start.cmd
+++ dist/windows/start.cmd
@@ -1,5 +1,10 @@
 @echo off
 
+cd "%~dp0"
+
+where /q /r "%programfiles%" vjoyconfig.exe 2> nul
+if %errorlevel% neq 0 start /wait cmd /c .\vjoy\setup.bat
+
 echo Server addresses:
 for /f "skip=3 tokens=1,* delims=:" %%g in ('ipconfig') do (
 	for /f "tokens=*" %%f in ('echo %%g ^| findstr "^[a-z]"') do (
@@ -11,11 +16,11 @@ for /f "skip=3 tokens=1,* delims=:" %%g in ('ipconfig') do (
 		echo | set /p=http://
 		echo | set /p=%%h
 		echo | set /p=:31415/
-		echo(
+		echo.
 	)
 )
-echo(
+echo.
 
 echo Starting server...
 .\websocketd\websocketd.exe --binary --port 31415 .\bin\ws_handler.exe
-
+echo.
---
+++ dist/windows/vjoy/setup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+cd "%~dp0"
+
+echo Installing vJoy driver...
+.\elevate.exe -wait .\vJoySetup.exe /silent
+echo.
+
+for /f "delims=" %%v in ('where /r "%programfiles%" vjoyconfig.exe') do (
+	for /l %%c in (1,1,16) do (
+		echo Configuring controller %%c...
+		.\elevate.exe -wait "%%v" %%c -f -a x y rx ry -b 16
+	)
+	echo.
+
+	pause
+	exit
+)
+echo.
+
+echo Setup incomplete. Please run setup again!
+pause
